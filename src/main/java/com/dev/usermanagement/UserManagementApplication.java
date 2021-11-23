2cf16195be8d76e9e4cbc8b1d9da5a0bde2b2156
Akila De Silva <akiladesilva97@gmail.com>
2021-11-24 02:05:07 +0530

    employee class and constructors

--- src/main/java/com/dev/usermanagement/UserManagementApplication.java
+++ src/main/java/com/dev/usermanagement/UserManagementApplication.java
@@ -1,9 +1,14 @@
 package com.dev.usermanagement;
 
+import com.dev.usermanagement.employee.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +19,8 @@ public class UserManagementApplication {
 	}
 
 	@GetMapping
-	public String hello(){
-		return "Hello World";
+	public List<Employee> hello(){
+		return List.of(new Employee(1L,"akila","akila@gmail.com", LocalDate.of(1997, Month.MARCH, 29), 21));
+
 	}
 }
